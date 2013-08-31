461b043484e284748789661f351f7c4c5024337c
Ehtesh Choudhury <merlockmagus@gmail.com>
2013-08-31 13:50:23 -0700

    Added center and clear button Converted some values to variables

--- application.js
+++ application.js
@@ -1,31 +1,17 @@
 //var map = L.map('map').setView([51.505, -0.09], 13);
-var southWest = new L.LatLng(34.018259,-118.291372),
-    northEast = new L.LatLng(34.026217,-118.278358),
+var southWest = new L.LatLng(34.01654, -118.2945),
+    northEast = new L.LatLng(34.03463, -118.27435),
+    center = new L.LatLng(34.02179, -118.2867);
     boundGalores = new L.LatLngBounds(southWest, northEast);
 var currentBuildings = {};
+var currentZoom = 16;
 
-var map = L.map('map').setView([34.02179, -118.2867], 17);
+var map = L.map('map', {center: center, zoom:currentZoom});
 L.tileLayer('http://{s}.tile.cloudmade.com/f8a4bd5801d64e6c8d0845c5b32ff0cd/997/256/{z}/{x}/{y}.png', {
-    minZoom: 16,
+    minZoom: 13,
     maxZoom: 18,
-    maxBounds: boundGalores,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 }).addTo(map);
-
-//L.marker([51.5, -0.09]).addTo(map)
-    //.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-//L.circle([51.508, -0.11], 500, {
-    //color: 'red',
-    //fillColor: '#f03',
-    //fillOpacity: 0.5
-//}).addTo(map).bindPopup("I am a circle.");
-
-//L.polygon([
-    //[51.509, -0.08],
-    //[51.503, -0.06],
-    //[51.51, -0.047]
-//]).addTo(map).bindPopup("I am a polygon.");
 
 // click event handler
 
@@ -43,7 +29,6 @@ function onMapClick(e) {
 function format(item) { return item.name + " (" + item.initials + ")"; }
 
 $.getJSON('buildings2.json', function(data) {
-    buildings = data;
     $("#buildings").select2({
         placeholder: "Select a building",
         data:{ results: data, text: 'name' },
@@ -66,6 +51,16 @@ $("#buildings").on("change", function(e){
         map.removeLayer(currentBuildings[e.removed.id]);
         delete currentBuildings[e.removed.id];
     }
+});
 
-    console.log(currentBuildings);
+$("#clear").on("click", function(){
+    for (var building in currentBuildings){
+        map.removeLayer(currentBuildings[building]);
+    }
+    currentBuildings = {};
+    $("#buildings").select2("val", "");
+});
+
+$("#center").on("click", function(){
+    map.panTo(center).setZoom(currentZoom);
 });
